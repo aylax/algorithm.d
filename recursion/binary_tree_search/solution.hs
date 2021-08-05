e41dd80d57b4239da586060efefb3903ba43ed49
aylax <zhoubye@foxmail.com>
2021-08-05 17:28:49 +0800

    new :: recursion (binary tree search) -> add haskell solution

--- recursion/binary_tree_search/solution.hs
+++ recursion/binary_tree_search/solution.hs
@@ -10,4 +10,13 @@ insert' x (Node a left right)
     | x < a = Node a (insert' x left) right
     | x > a = Node a left (insert' x right)
  
+search' :: (Ord a) => a -> Tree a -> Tree a
+search' x Nil = Nil
+search' x (Node a left right)
+    | x == a = Node a left right
+    | x < a = search' x left
+    | x > a = search' x right
+
+
+
 example_tree = foldr insert' Nil [8, 6, 3, 2, 1]
