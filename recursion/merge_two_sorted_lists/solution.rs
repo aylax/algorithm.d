3a5ac058577121c409f5fc4bdeb4e82111bd6022
aylax <zhoubye@foxmail.com>
2021-08-08 17:03:55 +0800

    new :: recursion(merge two sorted lists) -> add solution

--- recursion/merge_two_sorted_lists/solution.rs
+++ recursion/merge_two_sorted_lists/solution.rs
@@ -4,7 +4,7 @@
 //   pub val: i32,
 //   pub next: Option<Box<ListNode>>
 // }
-// 
+//
 // impl ListNode {
 //   #[inline]
 //   fn new(val: i32) -> Self {
@@ -15,7 +15,26 @@
 //   }
 // }
 impl Solution {
-    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        
+    pub fn merge_two_lists(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        so(l1, l2)
+    }
+}
+
+pub fn so(xas: Option<Box<ListNode>>, xbs: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    match (xas, xbs) {
+        (xs, None) => xs,
+        (None, xs) => xs,
+        (Some(mut sa), Some(mut sb)) => {
+            if sa.val < sb.val {
+                sa.next = so(sa.next.take(), Some(sb));
+                Some(sa)
+            } else {
+                sb.next = so(Some(sa), sb.next.take());
+                Some(sb)
+            }
+        }
     }
 }
