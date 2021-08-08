7efe419be3ccc9bc414176e0df2aa753205b5f3b
aylax <zhoubye@foxmail.com>
2021-08-09 06:32:49 +0800

    new :: recurstion (kth symbol in grammer) -> add question

---
+++ recursion/kth_symbol_in_grammar/solution.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn kth_grammar(n: i32, k: i32) -> i32 {
+        
+    }
+}
+
+/**
+ * 0
+ * 0 1
+ * 0 1 1 0
+ * 0 1 1 0 1 0 0 1
+ */
+pub fn so(n: i32, k: i32) -> i32 {
+
+}
