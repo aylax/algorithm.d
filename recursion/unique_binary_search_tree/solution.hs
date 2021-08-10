0d973569ac3ad4d2b60285076682988d9c2a9eec
aylax <zhoubye@foxmail.com>
2021-08-10 23:40:46 +0800

    fix :: recursion (unique binary search tree) -> fix haskell solution

--- recursion/unique_binary_search_tree/solution.hs
+++ recursion/unique_binary_search_tree/solution.hs
@@ -1,22 +1,39 @@
 data Tree a = Nil | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
 
-cart_prod xs ys = [(x,y) | x <- xs, y <- ys]
 
-make :: (Num a) => a -> [Tree a] -> [Tree a] -> [Tree a]
-make val xs ys = map (\x -> Node val (fst x) (snd x)) (cart_prod xs ys)
-
-echo :: (Num a) => Tree a -> [a]
-echo Nil = [-1]
-echo (Node a Nil Nil) = [a]
-echo (Node a left right) = [a] ++ (echo left) ++ (echo right)
+dp xs ys = [(x,y) | x <- xs, y <- ys]
 
 
-loop :: (Num a, Enum a, Ord a) => a -> a -> [Tree a]
-loop start end 
-  | start > end = [Nil]
-  | start == end = [Node start Nil Nil] 
-  | otherwise = foldr (++) [] [make n (loop start (n - 1)) (loop (n + 1) end) | n <- [start..end]]
+f :: (Num a) => a -> [Tree a] -> [Tree a] -> [Tree a]
+f k xs ys = map (\x -> Node k (fst x) (snd x)) (dp xs ys)
+
+
+g :: (Num a, Enum a, Ord a) => a -> a -> [Tree a]
+g a b
+  | a > b = [Nil]
+  | a == b = [Node a Nil Nil] 
+  | otherwise = foldr (++) [] [f n (g a (n - 1)) (g (n + 1) b) | n <- [a..b]]
+
+
+tick :: (Num a) => Tree a -> a
+tick Nil = -1
+tick (Node  a left right) = a
+
+
+pick :: (Num a) => Tree a -> [Tree a]
+pick Nil = []
+pick (Node a Nil Nil) = []
+pick (Node a n r) = [n, r]
+
+
+echo :: (Num a) => [Tree a] -> [a]
+echo [] = []
+echo xs = (map tick xs) ++ (echo (foldr (++) [] [pick n | n <- xs]))
 
 
 -- # GHCI
--- map echo (loop 1 3)
+-- map echo (g 1 3)
+
+
+ret = [[n] | n <- g 1 3]
+
