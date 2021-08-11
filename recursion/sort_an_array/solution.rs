d9883edf5a3bb718dcb57d5f65d58e27e232166a
aylax <zhoubye@foxmail.com>
2021-08-11 15:42:37 +0800

    new :: recursion (sort an array) -> add rust solution

--- recursion/sort_an_array/solution.rs
+++ recursion/sort_an_array/solution.rs
@@ -1,10 +1,35 @@
 impl Solution {
-    pub fn sort_array(nums: Vec<i32>) -> Vec<i32> {
-        
+    pub fn sort_array(mut xs: Vec<i32>) -> Vec<i32> {
+        let h = xs.len() as usize;
+        quick_sort(&mut xs, 0, h - 1);
+        xs
     }
 }
 
+pub fn quick_sort(xs: &mut Vec<i32>, l: usize, h: usize) {
+    if l < h {
+        let k = plot(xs, l, h);
+        if k > 1 {
+            quick_sort(xs, l, k - 1);
+        }
+        quick_sort(xs, k + 1, h);
+    }
+}
 
-pub fn so(xs: Vec<i32>) -> Vec<i32> {
+pub fn plot(xs: &mut Vec<i32>, l: usize, h: usize) -> usize {
+    let (mut i, mut j, e) = (l, h, xs[h]);
+    while i != j {
+        while i < j && xs[i] <= e {
+            i = i + 1;
+        }
+        while i < j && xs[j] >= e {
+            j = j - 1;
+        }
 
+        if i < j {
+            xs.swap(i, j);
+        }
+    }
+    xs.swap(i, h);
+    i
 }
