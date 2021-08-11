e56626fa1350ee5084a6d2f0a473580d51eab33e
aylax <zhoubye@foxmail.com>
2021-08-12 07:55:04 +0800

    new :: recursion (validate binary search tree) -> add solution

--- recursion/validate_binary_search_tree/solution.hs
+++ recursion/validate_binary_search_tree/solution.hs
@@ -1,31 +1,20 @@
 data Tree a = Nil | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
 
-
-val :: (Ord a) => Tree a -> a
-val (Node a left right) = a
-
-
-car :: (Ord a) => Tree a -> Tree a
-car Nil = Nil
-car (Node a left right) = left
+flat :: Tree a -> [a]
+flat Nil = []
+flat (Node a l r) = flat l ++ [a] ++ flat r
 
 
-cdr :: (Ord a) => Tree a -> Tree a
-cdr Nil = Nil
-cdr (Node a left right) = right
+g :: (Ord a) => [a] -> Bool
+g [] = True
+g [x] = True
+g (x:y:xs) = x < y && g (y:xs)
 
 
-sk :: (Ord a) => (a -> a -> Bool) -> a -> Tree a -> Bool
-sk f n Nil = True
-sk f n (Node a left right) = f a n
+
+so :: (Ord a) => Tree a -> Bool
+so root = g (flat root)
 
 
-check :: (Ord a) => Tree a -> Bool
-check Nil = True
-check root = sk (<) n l && sk (>) n r && check l && check r
-    where 
-        n = (val root)
-        l = (car root)
-        r = (cdr root)
-
+tree = Node 5 (Node 1 Nil Nil) (Node 4 (Node 3 Nil Nil) (Node 6 Nil Nil))
 
--- recursion/validate_binary_search_tree/solution.rs
+++ recursion/validate_binary_search_tree/solution.rs
@@ -19,35 +19,19 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-type RRT = Rc<RefCell<TreeNode>>;
-
 impl Solution {
     pub fn is_valid_bst(root: Option<RRT>) -> bool {
-        so(&root)
+        so(&root, std::i64::MIN, std::i64::MAX)
     }
 }
 
-pub fn so(root: &Option<RRT>) -> bool {
+pub fn so(root: &Option<RRT>, min: i64, max: i64) -> bool {
     match root {
         None => true,
         Some(rrt) => {
-            let node = rrt.borrow();
-            let (v, l, r) = (node.val, node.left, node.right);
-            cmp(gt, v, &l) && cmp(lt, v, &r) && so(&l) && so(&r)
+            let p = rrt.borrow();
+            let (n, l, r) = (p.val as i64, &p.left, &p.right);
+            min < n && n < max && so(l, min, n) && so(r, n, max)
         }
     }
-}
-
-fn gt(a: i32, b: i32) -> bool {
-    a > b
-}
-fn lt(a: i32, b: i32) -> bool {
-    a < b
-}
-
-pub fn cmp(op: fn(i32, i32) -> bool, val: i32, node: &Option<RRT>) -> bool {
-    match &node {
-        None => true,
-        Some(rrts) => op(rrts.borrow().val, val),
-    };
 }
